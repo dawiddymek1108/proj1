29f4ec77da8b0a9583bd30029acc426af779b87f
Dawid <dsw52667@student.dsw.edu.pl>
2024-06-29 13:51:13 +0200

    Dodano opcję --logs do skryptu

--- skrypt.sh
+++ skrypt.sh
@@ -2,3 +2,9 @@
 if [[ "$1" == "--date" ]]; then
   date
 fi
+if [[ "$1" == "--logs" ]]; then
+  num_files=${2:-100}
+  for i in $(seq 1 $num_files); do
+    echo -e "log$i\n$0\n$(date)" > log$i.txt
+  done
+fi
