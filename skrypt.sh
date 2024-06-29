eb4f176bde3260a2177368df129e9c0654f35693
Dawid <dsw52667@student.dsw.edu.pl>
2024-06-29 13:57:47 +0200

    Dodano flagi z pojedynczym myślnikiem

--- skrypt.sh
+++ skrypt.sh
@@ -14,3 +14,18 @@ if [[ "$1" == "--help" ]]; then
   echo "--logs [n] - tworzy n plików logx.txt (domyślnie 100)"
   echo "--help - wyświetla dostępne opcje"
 fi
+if [[ "$1" == "--date" || "$1" == "-d" ]]; then
+  date
+fi
+if [[ "$1" == "--logs" || "$1" == "-l" ]]; then
+  num_files=${2:-100}
+  for i in $(seq 1 $num_files); do
+    echo -e "log$i\n$0\n$(date)" > log$i.txt
+  done
+fi
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+  echo "Dostępne opcje:"
+  echo "--date, -d - wyświetla dzisiejszą datę"
+  echo "--logs [n], -l [n] - tworzy n plików logx.txt (domyślnie 100)"
+  echo "--help, -h - wyświetla dostępne opcje"
+fi
